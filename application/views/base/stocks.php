76f96723b1426c1919901f2993437f9ff2df4147
Adam Slater <adam.slater.1010@gmail.com>
2016-02-11 20:43:15 -0800

    Style on homepage - tables

--- application/views/base/stocks.php
+++ application/views/base/stocks.php
@@ -1,17 +1,21 @@
-<h2>Stocks</h2>
-<table class="table">
-    <tr>
-        <th>Code</th>
-        <th>Name</th>
-        <th>Category</th>
-        <th>Value</th>
-    </tr>
-    {stocks}
-    <tr>
-        <td><a href={href}>{Code}</a></td>
-        <td>{Name}</td>
-        <td>{Category}</td>
-        <td>{Value}</td>
-    </tr>
-    {/stocks}
+<h2 class="text-center underline">Stocks</h2>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Code</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Value</th>
+        </tr>
+    </thead>
+    <tbody>
+        {stocks}
+        <tr>
+            <td><a href={href}>{Code}</a></td>
+            <td>{Name}</td>
+            <td>{Category}</td>
+            <td>{Value}</td>
+        </tr>
+        {/stocks}
+    </tbody>
 </table>
