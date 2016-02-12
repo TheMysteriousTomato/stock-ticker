2be029b8f2713efbbe4d4c77bdb306ff75bcbdef
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-02-11 19:22:17 -0800

    Config: fix default index_page

--- application/config/config.php
+++ application/config/config.php
@@ -30,12 +30,12 @@ $config['base_url'] = '/';
 | Index File
 |--------------------------------------------------------------------------
 |
-| Typically this will be your players.php file, unless you've renamed it to
+| Typically this will be your index.php file, unless you've renamed it to
 | something else. If you are using mod_rewrite to remove the page set this
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'players.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
