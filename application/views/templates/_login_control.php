a5062e3939d22efe8131b90e185f50c706fca68c
Adam Slater <adam.slater.1010@gmail.com>
2016-04-09 17:42:36 -0700

    added password field

--- application/views/templates/_login_control.php
+++ application/views/templates/_login_control.php
@@ -1,6 +1,7 @@
 <form class='navbar-form navbar-right' action='/user/login' method='post'>
     <div class='form-group'>
-        <input name='username' type='text' class='form-control' placeholder='Username'>
+        <input name='username' type='text'     class='form-control' placeholder='Username'>
+        <input name='password' type='password' class='form-control' placeholder='Password'>
     </div>
     <button type='submit' class='btn btn-success'>Login</button>
 </form>
