51d61aafc7f5056a6daffc6bca520dd2780e2aa8
Adam Slater <adam.slater.1010@gmail.com>
2016-04-10 12:48:04 -0700

    added user roles constants

--- application/config/constants.php
+++ application/config/constants.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+define('ROLE_PLAYER', 'player');
+define('ROLE_ADMIN',  'admin');
+
 /*
 |--------------------------------------------------------------------------
 | Display Debug backtrace
