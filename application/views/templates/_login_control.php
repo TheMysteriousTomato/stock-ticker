96782fbcdfceed926a01e5203735bd9550cbd585
Adam Slater <adam.slater.1010@gmail.com>
2016-04-11 08:42:28 -0700

    added user login

--- application/views/templates/_login_control.php
+++ application/views/templates/_login_control.php
@@ -2,6 +2,9 @@
     <div class='form-group'>
         <input name='username' type='text'     class='form-control' placeholder='Username'>
         <input name='password' type='password' class='form-control' placeholder='Password'>
+        <span class="btn btn-default btn-file">
+            Upload <input type="file" name="avatar">
+        </span>
     </div>
     <button type='submit' class='btn btn-success'>Login</button>
 </form>
