d0fea1d9bb70a9fb8059d1a6662d389ac7d746a6
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-04-12 13:03:44 -0700

    Gameplay: update portfolio status

--- application/controllers/Gameplay.php
+++ application/controllers/Gameplay.php
@@ -10,6 +10,19 @@ class Gameplay extends Application {
     public function index()
     {
 
+        $playername = $this->session->userdata('username');
+        $player = $this->players->some('Player', $playername);
+
+        if(!(empty($player))) {
+            $player = $player[0];
+            $this->data['PlayerName'] = $playername;
+            $this->data['Cash'] = $player->Cash;
+            $this->data['Equity'] = $this->players->getEquity($playername);
+        }
+        else {
+           // TODO: If not logged in display error page??
+        }
+
         /* Set up data to render page */
         $this->data['title'] = "Gameplay";
         $this->data['left-panel-content'] = 'gameplay/status.php';
