365655c3938e288e60de705a1082eece2976ac4d
Adam Slater <adam.slater.1010@gmail.com>
2016-04-12 11:58:52 -0700

    fixed static player list in player management

--- application/views/management/player-managements.php
+++ application/views/management/player-managements.php
@@ -1,31 +1,31 @@
 <h2 class="text-center underline">Player Management</h2>
-
+<br>
 <ul class="list-group col-md-12">
-    <li class="list-group-item col-md-12">
+    <li class="list-group-item col-md-12 container">
         <div class="col-md-4">Player Name Here</div>
         <div class="col-md-4">
             <input type="checkbox" name="my-checkbox" data-on-text="Admin" data-off-text="Player" checked>
         </div>
         <div class="col-md-4">
-            <button class="btn btn-danger">Delete</button>
+            <button class="btn btn-danger pull-right">Delete</button>
         </div>
     </li>
-    <li class="list-group-item col-md-12">
+    <li class="list-group-item col-md-12 container">
         <div class="col-md-4">Player Name Here</div>
         <div class="col-md-4">
             <input type="checkbox" name="my-checkbox" data-on-text="Admin" data-off-text="Player" checked>
         </div>
         <div class="col-md-4">
-            <button class="btn btn-danger">Delete</button>
+            <button class="btn btn-danger pull-right">Delete</button>
         </div>
     </li>
-    <li class="list-group-item col-md-12">
+    <li class="list-group-item col-md-12 container">
         <div class="col-md-4">Player Name Here</div>
         <div class="col-md-4">
             <input type="checkbox" name="my-checkbox" data-on-text="Admin" data-off-text="Player" checked>
         </div>
         <div class="col-md-4">
-            <button class="btn btn-danger">Delete</button>
+            <button class="btn btn-danger pull-right">Delete</button>
         </div>
     </li>
 </ul>
