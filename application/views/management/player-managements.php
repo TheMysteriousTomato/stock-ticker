26917963772737ae6e793404aaa38ab020718bea
Adam Slater <adam.slater.1010@gmail.com>
2016-04-12 11:46:12 -0700

    added static player management

--- application/views/management/player-managements.php
+++ application/views/management/player-managements.php
@@ -1,9 +1,35 @@
 <h2 class="text-center underline">Player Management</h2>
 
-//TODO <br>
-
-list players
-<ul>
-    <li>switches for admin / player</li>
-    <li>delete player</li>
+<ul class="list-group col-md-12">
+    <li class="list-group-item col-md-12">
+        <div class="col-md-4">Player Name Here</div>
+        <div class="col-md-4">
+            <input type="checkbox" name="my-checkbox" data-on-text="Admin" data-off-text="Player" checked>
+        </div>
+        <div class="col-md-4">
+            <button class="btn btn-danger">Delete</button>
+        </div>
+    </li>
+    <li class="list-group-item col-md-12">
+        <div class="col-md-4">Player Name Here</div>
+        <div class="col-md-4">
+            <input type="checkbox" name="my-checkbox" data-on-text="Admin" data-off-text="Player" checked>
+        </div>
+        <div class="col-md-4">
+            <button class="btn btn-danger">Delete</button>
+        </div>
+    </li>
+    <li class="list-group-item col-md-12">
+        <div class="col-md-4">Player Name Here</div>
+        <div class="col-md-4">
+            <input type="checkbox" name="my-checkbox" data-on-text="Admin" data-off-text="Player" checked>
+        </div>
+        <div class="col-md-4">
+            <button class="btn btn-danger">Delete</button>
+        </div>
+    </li>
 </ul>
+
+<script>
+    $("[name='my-checkbox']").bootstrapSwitch('state', true);
+</script>
