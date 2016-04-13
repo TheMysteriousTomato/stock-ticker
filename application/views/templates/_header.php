577abf5d5891c3892549aaec583732b3844b9d34
Adam Slater <adam.slater.1010@gmail.com>
2016-04-13 15:57:12 -0700

    added minor style changes

--- application/views/templates/_header.php
+++ application/views/templates/_header.php
@@ -9,9 +9,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">
-                    The Mysterious Tomato ~ Stock Ticker
-                </a>
+                <a class="navbar-brand" href="/">Stock Ticker<i class="divider-vertical"></i></a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
