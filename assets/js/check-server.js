568e3dc25d79c5cd79668f867b3ddc1eabd50646
Adam Slater <adam.slater.1010@gmail.com>
2016-04-13 18:39:36 -0700

    game status is alive

---
+++ assets/js/check-server.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    setInterval(update, 500);
+    function update(){
+        $.get("/homepage/status", function(){}).done(
+            function(data) {
+                $("#status-round").html(data["round"]);
+                $("#status-state").html(data["desc"]);
+                $("#status-upcoming").html(data["upcoming"]);
+                $("#status-countdown").html(data["countdown"]);
+                console.log(data);
+            }).fail(function(err) {
+                alert("Something went wrong, check the log");
+                console.error(err);
+            }
+        );
+    }
+});
