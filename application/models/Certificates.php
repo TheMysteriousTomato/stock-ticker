2306f734682e9e782a2c237dfff7a90266fa1335
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2016-04-15 17:25:39 -0700

    update agent registration form

---
+++ application/models/Certificates.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khoanguyen
+ * Date: 2016-04-15
+ * Time: 4:48 PM
+ */
---
+++ application/views/gameplay/buyfail.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: khoanguyen
+ * Date: 2016-04-15
+ * Time: 5:22 PM
+ */
